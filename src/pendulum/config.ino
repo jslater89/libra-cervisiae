09ab7f9782e7ed6bb15b7cad710e1d4224bafd04
Jay Slater <jslater@sdgsystems.com>
2018-05-28 11:05:30 -0400

    Dummy out some config stuff

--- src/pendulum/config.ino
+++ src/pendulum/config.ino
@@ -1,3 +1,14 @@
+/**
+{
+  "delaySeconds": 100,
+  "hydrometerName": "01234567890123456789012345678901234567890123456789",
+  "fullVoltage": 4.20000000000001,
+  "wifiNetwork": "01234567890123456789012345678901234567890123456789",
+  "wifiPassword": "01234567890123456789012345678901234567890123456789",
+  "gravityCoefficients": [1.000000000001, 1.000000000001, 1.000000000001],
+  "temperatureCoefficients": [1.000000000001, 1.000000000001, 1.000000000001]
+}
+ */
 
 // How long between readings in hydrometer mode?
 int delaySeconds = 1800; // half an hour
@@ -20,3 +31,74 @@ String wifiPassword;
 // temperature->gravity offset polynomials?
 double gravityCoefficients[3];
 double temperatureCoefficients[3];
+
+int saveConfig() {
+  if(hydrometerName.length() > 50) {
+    return -1;
+  }
+  if(wifiNetwork.length() > 50) {
+    return -2;
+  }
+  if(wifiPassword.length() > 50) {
+    return -3;
+  }
+  if(delaySeconds > 4200) {
+    return -4;
+  }
+
+  // get string, save file
+}
+
+int loadConfig() {
+
+  // load file, get string, put into variables
+}
+
+/* decoding
+const size_t bufferSize = 2*JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(7) + 600;
+DynamicJsonBuffer jsonBuffer(bufferSize);
+
+const char* json = "{\"delaySeconds\":100,\"hydrometerName\":\"01234567890123456789012345678901234567890123456789\",\"fullVoltage\":4.20000000000001,\"wifiNetwork\":\"01234567890123456789012345678901234567890123456789\",\"wifiPassword\":\"01234567890123456789012345678901234567890123456789\",\"gravityCoefficient\":[1.000000000001,1.000000000001,1.000000000001],\"temperatureCoefficient\":[1.000000000001,1.000000000001,1.000000000001]}";
+
+JsonObject& root = jsonBuffer.parseObject(json);
+
+int delaySeconds = root["delaySeconds"]; // 100
+const char* hydrometerName = root["hydrometerName"]; // "01234567890123456789012345678901234567890123456789"
+float fullVoltage = root["fullVoltage"]; // 4.20000000000001
+const char* wifiNetwork = root["wifiNetwork"]; // "01234567890123456789012345678901234567890123456789"
+const char* wifiPassword = root["wifiPassword"]; // "01234567890123456789012345678901234567890123456789"
+
+JsonArray& gravityCoefficient = root["gravityCoefficient"];
+float gravityCoefficient0 = gravityCoefficient[0]; // 1.000000000001
+float gravityCoefficient1 = gravityCoefficient[1]; // 1.000000000001
+float gravityCoefficient2 = gravityCoefficient[2]; // 1.000000000001
+
+JsonArray& temperatureCoefficient = root["temperatureCoefficient"];
+float temperatureCoefficient0 = temperatureCoefficient[0]; // 1.000000000001
+float temperatureCoefficient1 = temperatureCoefficient[1]; // 1.000000000001
+float temperatureCoefficient2 = temperatureCoefficient[2]; // 1.000000000001
+*/
+
+/* encoding
+const size_t bufferSize = 2*JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(7);
+DynamicJsonBuffer jsonBuffer(bufferSize);
+
+JsonObject& root = jsonBuffer.createObject();
+root["delaySeconds"] = 100;
+root["hydrometerName"] = "01234567890123456789012345678901234567890123456789";
+root["fullVoltage"] = 4.20000000000001;
+root["wifiNetwork"] = "01234567890123456789012345678901234567890123456789";
+root["wifiPassword"] = "01234567890123456789012345678901234567890123456789";
+
+JsonArray& gravityCoefficient = root.createNestedArray("gravityCoefficient");
+gravityCoefficient.add(1.000000000001);
+gravityCoefficient.add(1.000000000001);
+gravityCoefficient.add(1.000000000001);
+
+JsonArray& temperatureCoefficient = root.createNestedArray("temperatureCoefficient");
+temperatureCoefficient.add(1.000000000001);
+temperatureCoefficient.add(1.000000000001);
+temperatureCoefficient.add(1.000000000001);
+
+root.printTo(Serial);
+ */
