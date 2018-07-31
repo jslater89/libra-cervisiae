47aac6b98647e828150cc492a8fb3acde41a59f8
Jay Slater <jslater@sdgsystems.com>
2018-07-31 17:38:14 -0400

    Groovy logger, move dev-chrome script out of data
    The data dir goes onto the ESP flash, remember.

---
+++ src/libra-cervisiae/groovyLogger.groovy
@@ -0,0 +1,16 @@
+println "sequence#,date,boardTemp,rawWeight,calibratedWeight"
+
+int seq = 0;
+
+while(true) {
+    def connection = new URL("http://jarvis.local/live").openConnection() as HttpURLConnection
+
+    def text = connection.inputStream.text;
+    def json = new groovy.json.JsonSlurper().parseText(text);
+
+    println "${seq++},${new Date().format("yyyyMMdd-HHmmss")},$json.boardTemperature,$json.rawWeight,$json.calibratedWeight"
+
+    sleep(1000 * 60 * 30);
+}
+
+
