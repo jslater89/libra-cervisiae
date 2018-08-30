4341a6195ee76034f883266bd89535b7002e4427
Jay Slater <jslater@sdgsystems.com>
2018-08-30 13:29:47 -0400

    Ignore exceptions in groovyLogger

--- src/libra-cervisiae/groovyLogger.groovy
+++ src/libra-cervisiae/groovyLogger.groovy
@@ -3,12 +3,15 @@ println "sequence#,date,boardTemp,rawWeight,calibratedWeight,wortTemp,abw,abv,gr
 int seq = 0;
 
 while(true) {
-    def connection = new URL("http://jarvis.local/live").openConnection() as HttpURLConnection
+    try {
+        def connection = new URL("http://vision.local/live").openConnection() as HttpURLConnection
 
-    def text = connection.inputStream.text;
-    def json = new groovy.json.JsonSlurper().parseText(text);
+        def text = connection.inputStream.text;
+        def json = new groovy.json.JsonSlurper().parseText(text);
 
-    println "${seq++},${new Date().format("MM/dd/yyyy HH:mm:ss")},$json.boardTemperature,$json.rawWeight,$json.calibratedWeight,$json.wortTemperature,$json.abw,$json.abv,$json.gravity"
+        println "${seq++},${new Date().format("MM/dd/yyyy HH:mm:ss")},$json.boardTemperature,$json.rawWeight,$json.calibratedWeight,$json.wortTemperature,$json.abw,$json.abv,$json.gravity"
+    }
+    catch(Exception e) {}
 
     sleep(1000 * 60 * 10);
 }
